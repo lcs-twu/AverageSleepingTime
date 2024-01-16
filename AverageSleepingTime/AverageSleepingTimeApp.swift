0fe4041a30b2868ab305fc49b2bd99fdeae6d2c4
Tom Wu <twu@lcs.on.ca>
2024-01-16 09:11:37 -0500

    Initial Commit

---
+++ AverageSleepingTime/AverageSleepingTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AverageSleepingTimeApp.swift
+//  AverageSleepingTime
+//
+//  Created by Tom Wu on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct AverageSleepingTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
